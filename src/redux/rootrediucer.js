597cb2c957c679cade80787c826a0f9d330cdecb
unknown <martirosyannarek844@gmail.com>
2026-03-09 14:19:34 +0400

    fix bag

--- src/redux/rootrediucer.js
+++ src/redux/rootrediucer.js
@@ -1,9 +1,10 @@
 import { combineReducers } from "redux";
 import { likeRediucer } from "./about/rediucer";
 
+
 const rootRediucer = combineReducers({
-    text: likeRediucer,
-    likes: likeRediucer
+    likes: likeRediucer,
+    text: likeRediucer
 })
 
 export default rootRediucer
